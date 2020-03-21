3f2f8280846b3f1aee6fa7a59c5d8672239be0c9
wangj <icewind4096@gmail.com>
2020-03-21 10:16:08 +0800

    完成全局异常捕获处理

---
+++ src/main/java/com/windvalley/emall/common/ExceptionResolver.java
@@ -0,0 +1,25 @@
+package com.windvalley.emall.common;
+
+import com.windvalley.emall.enums.ResponseCode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@Component
+public class ExceptionResolver implements HandlerExceptionResolver {
+    @Override
+    public ModelAndView resolveException(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) {
+        log.info("{} exception", httpServletRequest.getRequestURI(), e);
+        ModelAndView modelAndView = new ModelAndView(new MappingJackson2JsonView());
+        modelAndView.addObject("status", ResponseCode.ERROR.getCode());
+        modelAndView.addObject("msg", "接口异常");
+        modelAndView.addObject("data", e.toString());
+        return modelAndView;
+    }
+}
